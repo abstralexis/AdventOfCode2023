b4bd7af88eff3983f489f0c525fa01e09a47d583
Alexis Williams <Thecomixguy@hotmail.com>
2023-12-01 19:38:23 +0000

    Day 1 Part 2

--- src/day1.rs
+++ src/day1.rs
@@ -39,13 +39,33 @@
 ///
 ///Consider your entire calibration document. 
 ///What is the sum of all of the calibration values?
+///
+///  --- Part Two ---
+///
+/// Your calculation isn't quite right. It looks like some of the digits are actually spelled out with letters: one, two, three, four, five, six, seven, eight, and nine also count as valid "digits".
+///
+/// Equipped with this new information, you now need to find the real first and last digit on each line. For example:
+///
+/// two1nine
+/// eightwothree
+/// abcone2threexyz
+/// xtwone3four
+/// 4nineeightseven2
+/// zoneight234
+/// 7pqrstsixteen
+///
+/// In this example, the calibration values are 29, 83, 13, 24, 42, 14, and 76. Adding these together produces 281.
+///
+/// What is the sum of all of the calibration values?
 
-use std::include_str;
+
+use std::{include_str, collections::HashMap};
 
 pub fn solution() -> i32 {
-    include_str!("day1.txt")
+    dbg!(numeric_insertions(include_str!("day1.txt")))
     .lines()
     .map(|line| {
+        &line;
         format!("{}{}",
             line.chars().nth(line.find(char::is_numeric).unwrap()).unwrap(), 
             line.chars().nth(line.rfind(char::is_numeric).unwrap()).unwrap()
@@ -54,6 +74,26 @@ pub fn solution() -> i32 {
     .fold(0, |acc, strnum| {
         acc + strnum.parse::<i32>().unwrap()
     })
+}
+
+pub fn numeric_insertions<'a>(s: &'a str) -> String {
+    let mut string = s.to_owned();
+    let map = HashMap::from([
+        ("one", "1"),
+        ("two", "2"),
+        ("three", "3"),
+        ("four", "4"),
+        ("five", "5"),
+        ("six", "6"),
+        ("seven", "7"),
+        ("eight", "8"),
+        ("nine", "9"),
+    ]);
+    for (word, num) in map.iter() {
+        let replacer = dbg!(format!("{word}{num}{word}"));
+        string = string.replace(word, &replacer);
+    }
+    string
 }
 
 #[test]
